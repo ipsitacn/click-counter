371ee4ccbbe07171812cdef7f82980f6b6537533
ipsitacn <ipsita.mallick@capitalnumbers.com>
2021-05-19 17:20:08 +0530

    testing updated

--- src/App.js
+++ src/App.js
@@ -1,10 +1,45 @@
-import logo from "./logo.svg";
+import { useState } from "react";
 import "./App.css";
 
 function App() {
+  const [count, setCount] = useState(0);
+  const [error, setError] = useState(false);
+
   return (
     <div data-test="component-app">
-      <h1>App</h1>
+      <h1 data-test="counter-display">
+        The counter is currently&nbsp;
+        <span data-test="count">{count}</span>
+      </h1>
+      <div
+        data-test="error-message"
+        className={`error ${error ? "" : "hidden"}`}
+      >
+        The counter cannot go below 0
+      </div>
+      <button
+        data-test="increment-button"
+        onClick={() => {
+          if (error) {
+            setError(false);
+          }
+          setCount(count + 1);
+        }}
+      >
+        Increment Counter
+      </button>
+      <button
+        data-test="decrement-button"
+        onClick={() => {
+          if (count > 0) {
+            setCount(count - 1);
+          } else {
+            setError(true);
+          }
+        }}
+      >
+        Decrement Counter
+      </button>
     </div>
   );
 }
